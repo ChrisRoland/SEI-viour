ec42475da69eab78dac82769b8d1663d7c468367
ChrisRoland <chrisebuberoland@gmail.com>
2024-01-11 21:10:28 +0100

    Add: About Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import WebComponent from './components/WebComponent';
+import About from './components/About';
 
 function App() {
   return (
     <>
-      <WebComponent />
+      <Routes>
+          <Route path="/" element={<WebComponent />} />
+          <Route path="/About" element={<About />} />
+      </Routes>
     </>
   );
 }
